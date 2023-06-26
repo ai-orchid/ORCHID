46b5eb07628847716fb329f38d5e255bbb1dcc1c
Aakash Rao <mrao.aakash@gmail.com>
2023-06-26 14:12:54 +0530

    Added new files

---
+++ build_dataframe.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# conda init bash
+# conda activate orchid
+python3 ./tools/build_df.py /Users/mraoaakash/Documents/research/research-nisha/ORCHID_data/ORCHID_data/ORCHID_TRS_working
+python3 ./training/test_data_loader.py /Users/mraoaakash/Documents/research/research-nisha/ORCHID_data/ORCHID_data/ORCHID_TRS_working DenseNet121
--- data_retrieve.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-conda init bash
-conda activate orchid
-python3 ./tools/data_retrieve.py
