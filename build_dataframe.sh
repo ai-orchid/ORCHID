b92e2a1332722c2f4c3be03d742ca5d5d750c891
Aakash Rao <mrao.aakash@gmail.com>
2023-06-26 14:37:49 +0530

    Added new files

--- build_dataframe.sh
+++ build_dataframe.sh
@@ -2,4 +2,4 @@
 # conda init bash
 # conda activate orchid
 python3 ./tools/build_df.py /home/chs.gpu/Documents/rintu/researchxoscc/project-1/train
-# python3 ./training/test_data_loader.py /home/chs.gpu/Documents/rintu/researchxoscc/project-1/train DenseNet121
+python3 ./training/test_data_loader.py /home/chs.gpu/Documents/rintu/researchxoscc/project-1/train DenseNet121
