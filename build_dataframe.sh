c212ec07b11344b60245859974b7c1b0e0c91e91
Aakash Rao <mrao.aakash@gmail.com>
2023-07-11 14:44:42 +0530

    added files

--- build_dataframe.sh
+++ build_dataframe.sh
@@ -2,4 +2,4 @@
 # conda init bash
 # conda activate orchid
 # python3 ./tools/build_df.py /home/chs.gpu/Documents/rintu/researchxoscc/project-1/train
-python3 ./tools/build_df.py /home/chs.gpu/Documents/rintu/researchxoscc/project-1/train 42
+python3 ./tools/build_df.py /home/chs.gpu/Documents/rintu/researchxoscc/project-1/train 1
