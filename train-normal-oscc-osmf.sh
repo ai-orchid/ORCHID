ab36d199e1d9f9f4e5c53e6a71b2ecbadaa43518
Aakash Rao <mrao.aakash@gmail.com>
2023-07-11 14:19:57 +0530

    added files

--- train-normal-oscc-osmf.sh
+++ train-normal-oscc-osmf.sh
@@ -1,2 +1,2 @@
 # python3 ./training/test_data_loader.py /home/chs.gpu/Documents/rintu/researchxoscc/project-1/train DenseNet121
-python3 ./training/train-classify-normal-oscc-osmf.py /Users/mraoaakash/Documents/research/research-nisha/ORCHID_data/ORCHID_data/data DenseNet121
+python3 ./training/train-classify-normal-oscc-osmf.py /home/chs.gpu/Documents/rintu/researchxoscc/project-1/train DenseNet121
